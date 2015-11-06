49db2495cbbe3556505fb01a57800f12f2e6be71
LaurentQUERSIN <quersin.laurent@gmail.com>
2015-11-06 14:49:03 +0100

    fixed login

--- The Interpolator/ConnectionDTO.cs
+++ The Interpolator/ConnectionDTO.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Interpolator
+﻿namespace Interpolator
 {
-    class ConnectionDTO
+    public class ConnectionDTO
     {
         public string version;
         public string name;
