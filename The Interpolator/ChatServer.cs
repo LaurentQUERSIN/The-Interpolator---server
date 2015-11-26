85b7567e86f6bbd95908918e8d2c267a38a9755c
LaurentQUERSIN <quersin.laurent@gmail.com>
2015-11-26 16:48:05 +0100

    added chat pulgin

---
+++ The Interpolator/ChatServer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Stormancer;
+using Stormancer.Core;
+using Stormancer.Plugins;
+using Stormancer.Server;
+
+public class ChatUserInfo
+{
+    public string User;
+}
+
+public struct ChatMessageDTO
+{
+    public ChatUserInfo UserInfo;
+    public string Message;
+}
+
+public static class ChatServerExtensions
+{
+    public static void AddChat(this ISceneHost scene)
+    {
+        ChatServer.ChatRoomBuilder(scene);
+    }
+}
+
+public class ChatServer
+{
+    void Run(IAppBuilder builder)
+    {
+        builder.SceneTemplate("ChatRoom", ChatRoomBuilder);
+    }
+
+     public static void ChatRoomBuilder(ISceneHost scene)
+    {
+        scene.AddRoute("chat", p => OnMessageReceived(scene, p));
+    }
+
+    static void OnMessageReceived(ISceneHost scene, Packet<IScenePeerClient> packet)
+    {
+        var dto = new ChatMessageDTO();
+        dto.UserInfo = packet.Connection.GetUserData<ChatUserInfo>();
+        dto.Message = packet.ReadObject<string>();
+        scene.Broadcast("chat", dto, PacketPriority.MEDIUM_PRIORITY, PacketReliability.RELIABLE);
+    }
+}
--- The Interpolator/Run.cs
+++ The Interpolator/Run.cs
@@ -9,7 +9,17 @@ using System;
 
 namespace Interpolator
 {
-    class InterpolatorScene
+    public class SceneRun
+    {
+        public void Run(IAppBuilder builder)
+        {
+            InterpolatorScene temp = new InterpolatorScene();
+            builder.SceneTemplate("interpolator_scene", temp.InterpolatorSceneBuilder);
+        }
+    }
+
+
+    public class InterpolatorScene
     {
         private ISceneHost _scene;
         private ILogger _log;
@@ -22,202 +32,15 @@ namespace Interpolator
 
         private ReplicatorBehaviour replicator = new ReplicatorBehaviour();
 
-        public void Run(IAppBuilder builder)
-        {
-            builder.SceneTemplate("interpolator_scene", InterpolatorSceneBuilder);
-        }
 
-        private void InterpolatorSceneBuilder(ISceneHost scene)
+
+        public void InterpolatorSceneBuilder(ISceneHost scene)
         {
             _scene = scene;
             _log = _scene.GetComponent<ILogger>();
             _env = _scene.GetComponent<IEnvironment>();
-           // _scene.Connecting.Add(OnConnecting);
-           // _scene.Connected.Add(OnConnected);
-           // _scene.Disconnected.Add(OnDisconnected);
-           // _scene.Starting.Add(OnStarting);
-           // _scene.Shuttingdown.Add(OnSD);
+            ChatServerExtensions.AddChat(_scene);
             replicator.Init(_scene);
-           // _scene.AddRoute("update_position", OnUpdatePosition);
-           // _scene.AddRoute("chat", OnChat);
-        }
-
-        private Task OnConnecting(IScenePeerClient client)
-        {
-            ConnectionDTO cdto = client.GetUserData<ConnectionDTO>();
-            _log.Debug("interpolator", "new client connecting :: name = " + cdto.name + " :: " + cdto.version);
-            if (cdto.version != version)
-                throw new ClientException("Wrong version");
-            if (_players.Count > 15)
-                throw new ClientException("server full");
-            foreach(Player p in _players.Values)
-            {
-                if (cdto.name == p.name)
-                    throw new ClientException("nickname already in use");
-            }
-            return Task.FromResult(true);
-        }
-
-        private async Task OnConnected(IScenePeerClient client)
-        {
-            ConnectionDTO cdto = client.GetUserData<ConnectionDTO>();
-
-            client.Send("get_id", w =>
-            {
-                var writer = new BinaryWriter(w, System.Text.Encoding.UTF8, false);
-                writer.Write(client.Id);
-            }, PacketPriority.HIGH_PRIORITY, PacketReliability.RELIABLE);
-            await Task.Delay(100);
-            client.Send("create_player",w =>
-            {
-                var writer = new BinaryWriter(w, System.Text.Encoding.UTF8, false);
-                foreach (Player p in _players.Values)
-                {
-                    writer.Write(p.Id);
-                }
-            }, PacketPriority.MEDIUM_PRIORITY, PacketReliability.RELIABLE);
-            _scene.Broadcast("create_player", w =>
-            {
-                var writer = new BinaryWriter(w, System.Text.Encoding.UTF8, false);
-                writer.Write(client.Id);
-            });
-            _scene.Broadcast("chat", cdto.name + " has connected.");
-            _players.TryAdd(client.Id, new Player(cdto.name, client.Id));
-        }
-
-        private Task OnDisconnected(DisconnectedArgs args)
-        {
-            _log.Debug("interpolator", args.Peer.GetUserData<ConnectionDTO>().name + "has disconnected. reason: " + args.Reason);
-            Player p;
-            _players.TryRemove(args.Peer.Id, out p);
-            _scene.Broadcast("chat", p.name + " has disconnected. (" + args.Reason + ")");
-            _scene.Broadcast("remove_player", w =>
-            {
-                var writer = new BinaryWriter(w, System.Text.Encoding.UTF8, false);
-                writer.Write(args.Peer.Id);
-            });
-            return Task.FromResult(true);
-        }
-
-        private void OnChat(Packet<IScenePeerClient> packet)
-        {
-            string message = packet.ReadObject<string>();
-            Player p;
-            if (_players.TryGetValue(packet.Connection.Id, out p))
-            {
-                message = p.name + " :" + message;
-                _scene.Broadcast("chat", message);
-            }
-        }
-
-        private long _lastStamp = 0;
-
-        private void OnUpdatePosition(Packet<IScenePeerClient> packet)
-        {
-            using (var reader = new BinaryReader(packet.Stream))
-            {
-                var id = reader.ReadInt64();
-                var stamp = reader.ReadInt64();
-                var x = reader.ReadSingle();
-                var y = reader.ReadSingle();
-                var z = reader.ReadSingle();
-
-                var vx = reader.ReadSingle();
-                var vy = reader.ReadSingle();
-                var vz = reader.ReadSingle();
-
-                var rx = reader.ReadSingle();
-                var ry = reader.ReadSingle();
-                var rz = reader.ReadSingle();
-                var rw = reader.ReadSingle();
-
-                Player p;
-                if (_players.TryGetValue(id, out p) && p != null && stamp > p._lastStamp)
-                {
-                    p._lastStamp = stamp;
-                    p.lastUpdate = _env.Clock;
-                    p.UpdatePosition(x, y, z, vx, vy, vz, rx, ry, rz, rw);
-                    _scene.Broadcast("update_position", w =>
-                    {
-                        using (var writer = new BinaryWriter(w, System.Text.Encoding.UTF8, true))
-                        {
-
-                            writer.Write(p.Id);
-                            writer.Write(p.lastUpdate);
-                            writer.Write(p.x);
-                            writer.Write(p.y);
-                            writer.Write(p.z);
-
-                            writer.Write(p.vx / 1000);
-                            writer.Write(p.vy / 1000);
-                            writer.Write(p.vz / 1000);
-
-                            writer.Write(p.rx);
-                            writer.Write(p.ry);
-                            writer.Write(p.rz);
-                            writer.Write(p.rw);
-                        }
-                    }, PacketPriority.MEDIUM_PRIORITY, PacketReliability.UNRELIABLE);
-                }
-            }
-        }
-
-        private Task _gameLoop;
-
-        private Task OnStarting(dynamic args)
-        {
-            _log.Debug("Interpolator", "scene succefully loaded, starting game loop...");
-            _gameLoop = StartGameLoop();
-            return Task.FromResult(true);
-        }
-
-        private async Task OnSD(ShutdownArgs args)
-        {
-            _log.Debug("Interpolator", "the scene shuts down: reason: " + args.Reason);
-            _isRunning = false;
-            try
-            {
-                await _gameLoop;
-            }
-            catch (Exception e)
-            {
-                _log.Log(LogLevel.Error, "runtimeError", "an error occurred in the game loop", e);
-            }
-        }
-
-        private async Task StartGameLoop()
-        {
-            _isRunning = true;
-            while (_isRunning == true)
-            {
-                //_scene.Broadcast("update_position", w =>
-                //{
-                //    var writer = new BinaryWriter(w, System.Text.Encoding.UTF8, false);
-
-                //    foreach(Player p in _players.Values)
-                //    {
-                //        if (p.lastUpdate < _env.Clock - 200)
-                //        {
-                //            writer.Write(p.Id);
-                //            writer.Write(_env.Clock);
-                //            writer.Write(p.x);
-                //            writer.Write(p.y);
-                //            writer.Write(p.z);
-
-                //            writer.Write(p.vx);
-                //            writer.Write(p.vy);
-                //            writer.Write(p.vz);
-
-                //            writer.Write(p.rx);
-                //            writer.Write(p.ry);
-                //            writer.Write(p.rz);
-                //            writer.Write(p.rw);
-                //        }
-                //    }
-
-                //}, PacketPriority.MEDIUM_PRIORITY, PacketReliability.UNRELIABLE);
-                await Task.Delay(200);
-            }
         }
     }
 }
